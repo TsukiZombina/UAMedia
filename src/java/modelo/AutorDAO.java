5a8578296aa82b9d6492249a70ff411ae8da8cb4
Luis Toral <Luis Toral@Personal-L>
2016-07-04 20:41:46 -0500

    Identación

--- src/java/modelo/AutorDAO.java
+++ src/java/modelo/AutorDAO.java
@@ -7,23 +7,23 @@ import java.sql.Statement;
 
 public class AutorDAO {
 	
-	public static boolean agregarAutor(AutorVO unAutor){
-		boolean agregado=false;
-		try {
-			ConexionBD c=new ConexionBD();
-			Connection con=c.getConexion();
-			if(con!=null){
-				Statement st;
-				st = con.createStatement();
-				st.executeUpdate("INSERT INTO alumno(`matricula`,`nombre`,`apellidos`, `correo`,`carrera`) VALUES ('"+unAutor.getMatricula()+"','"+unAutor.getNombre()+"','"+unAutor.getApellidos()+"','"+unAutor.getCorreo()+"','"+unAutor.getCarrera()+"')");
-				agregado=true;
-				st.close();
-			}
-			c.cerrarConexion();
-		} catch (SQLException e) {
-			agregado=false;
-			e.printStackTrace();
-		}
-		return agregado;
-	}
+    public static boolean agregarAutor(AutorVO unAutor){
+        boolean agregado=false;
+        try {
+        ConexionBD c=new ConexionBD();
+        Connection con=c.getConexion();
+            if(con!=null){
+                    Statement st;
+                    st = con.createStatement();
+                    st.executeUpdate("INSERT INTO alumno(`matricula`,`nombre`,`apellidos`, `correo`,`carrera`) VALUES ('"+unAutor.getMatricula()+"','"+unAutor.getNombre()+"','"+unAutor.getApellidos()+"','"+unAutor.getCorreo()+"','"+unAutor.getCarrera()+"')");
+                    agregado=true;
+                    st.close();
+            }
+        c.cerrarConexion();
+        } catch (SQLException e) {
+            agregado=false;
+            e.printStackTrace();
+        }
+        return agregado;
+    }
 }
